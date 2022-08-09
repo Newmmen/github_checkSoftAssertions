421211864569f943273c95701f5961a608a314f7
Newmmen <108897467+Newmmen@users.noreply.github.com>
2022-08-10 00:40:07 +0300

    edit checkSoftAssertions

--- src/test/java/com/github/CheckSoftAssertions.java
+++ src/test/java/com/github/CheckSoftAssertions.java
@@ -3,8 +3,8 @@ package com.github;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.Condition.enabled;
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.withTagAndText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -17,11 +17,10 @@ public class CheckSoftAssertions {
     @Test
     void checkSoftAssertions() {
         $("[placeholder='Search GitHub']").setValue("selenide").pressEnter();
-        $$("ul.repo-list li").first().$("a").click();
+        $("ul.repo-list li").$("a").click();
         $("#wiki-tab").click();
         $("#wiki-pages-filter").setValue("SoftAssertions");
         $$("[data-filterable-for='wiki-pages-filter']").first().shouldHave(text("SoftAssertions")).click();
-        $("div.markdown-body").$(withTagAndText("h4", "JUnit5")).shouldBe(enabled);
-
+        $("div.markdown-body").$(withTagAndText("h4", "JUnit5")).shouldBe(visible);
     }
 }
